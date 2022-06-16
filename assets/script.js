5bae058938b769acfc18c08393be384569ed2828
Evan Hardek <evanhardek@gmail.com>
2022-06-16 19:46:47 -0400

    added comments to html and script

--- assets/script.js
+++ assets/script.js
@@ -7,8 +7,16 @@ const recentSearchEl = document.getElementById('recentSearch')
 const currentEl = document.getElementById('current')
 const forecastEl = document.getElementById('forecast')
 
-
-
+// TODO: Create forecast fetch function
+// TODO: Create coords fetch function
+// TODO: Create event handlers
+// - Search Input Handler
+// - Recent search click handler 
+// TODO: Render Forecast function
+// TODO: Render Current function 
+// TODO: Create Save Recent Searches
+//  - Render all recent searches on load 
+//  - Recent searchs can be clicked to be searched again 
 
 
 
