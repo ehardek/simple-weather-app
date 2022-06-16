fb1572f90c5b611738a8a06bdb22c33ca17795d3
Evan Hardek <evanhardek@gmail.com>
2022-06-16 19:21:01 -0400

    queried necessary elements

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,13 @@
+const ApiRoot = 'https://api.openweathermap.org'
+const ApiKey = '2af0ef1a0f66a04443807e8de444eb4d'
+
+const searchFormEl = document.getElementById('citySearch')
+const searchInputEl = document.getElementById('cityName')
+const recentSearchEl = document.getElementById('recentSearch')
+const currentEl = document.getElementById('current')
+const forecastEl = document.getElementById('forecast')
 
 
-"https://api.openweathermap.org/data/2.5/weather?q="+ cityName + "&appid=2af0ef1a0f66a04443807e8de444eb4d"
 
 
 
