81dc88ccc4738c6aec891a869a1da4d41bbb67b6
Evan Hardek <evanhardek@gmail.com>
2022-06-17 00:32:14 -0400

    added recentClickHandler and renderRecentSearches

--- assets/script.js
+++ assets/script.js
@@ -3,23 +3,23 @@ const APIKEY = '2af0ef1a0f66a04443807e8de444eb4d'
 
 const searchFormEl = document.getElementById('citySearch')
 const searchInputEl = document.getElementById('cityName')
-const recentSearchEl = document.getElementById('recentSearch')
+const recentSearchEl = document.getElementById('recentSearchs')
 const currentEl = document.getElementById('currentContainer')
 const forecastEl = document.getElementById('forecastContainer')
 
 // TODO: Create forecast fetch function
 function weatherCall(location) {
-    var {lat, lon} = location
+    var { lat, lon } = location
     var city = location.name
     var weatherUrl = `${APIROOT}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKEY}`
     console.log(weatherUrl)
     fetch(weatherUrl)
-      .then((res)=>{ return res.json()})
-      .then((data)=>{
-         var {current, daily} = data
-         renderForecastContainer(daily)
-         renderCurrentContent(current, city)
-      })
+        .then((res) => { return res.json() })
+        .then((data) => {
+            var { current, daily } = data
+            renderForecastContainer(daily)
+            renderCurrentContent(current, city)
+        })
 }
 // TODO: Create coords fetch function
 function coordsCall(search) {
@@ -38,7 +38,7 @@ function coordsCall(search) {
 }
 // TODO: Create event handlers  
 // - Search Input Handler
-function searchHandler(e) {
+function searchSubmitHandler(e) {
     if (!searchInputEl.value) {
         return alert('Please Enter A CityName');
     }
@@ -49,25 +49,29 @@ function searchHandler(e) {
     searchInputEl.value = ''
 }
 // - Recent search click handler 
-
+function recentClickHandler(e){
+    e.preventDefault()
+   var button = e.target
+   var search = button.textContent
+   coordsCall(search)
+}
 // TODO: Render Forecast functions
-function renderForecastContainer(daily){
+function renderForecastContainer(daily) {
     forecastEl.innerHTML = ''
     var forecastHeading = document.createElement('h5')
-    forecastHeading.setAttribute('id','forecastHeading')
+    forecastHeading.setAttribute('id', 'forecastHeading')
     forecastHeading.textContent = `5-Day Forecast`
     forecastEl.append(forecastHeading);
-    daily.forEach((forecast)=>{renderForecastCard(forecast)})
+    daily.forEach((forecast) => { renderForecastCard(forecast) })
 }
-
-function renderForecastCard(forecast){
+function renderForecastCard(forecast) {
     var tempMax = forecast.temp.max
     var tempMin = forecast.temp.min
     var humidity = forecast.humidity
     var weather = forecast.weather[0].main
     var iconURL = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`
     var iconAlt = forecast.weather[0].icon
-    
+
     let card = document.createElement('div')
     let tempMinEl = document.createElement('p')
     let tempMaxEl = document.createElement('p')
@@ -75,17 +79,17 @@ function renderForecastCard(forecast){
     let humidityEl = document.createElement('p')
     let iconEl = document.createElement('img')
 
-    card.append(tempMaxEl,tempMinEl,iconEl,weatherEl,humidityEl)
+    card.append(tempMaxEl, tempMinEl, iconEl, weatherEl, humidityEl)
 
-    card.setAttribute('class','forecastCard')
-    tempMaxEl.setAttribute('class','cardTemp')
-    tempMinEl.setAttribute('class','cardTemp')
-    weatherEl.setAttribute('class','cardWeather')
-    humidityEl.setAttribute('class','cardHumidity')
-    iconEl.setAttribute('class','cardIcon')
+    card.setAttribute('class', 'forecastCard')
+    tempMaxEl.setAttribute('class', 'cardTemp')
+    tempMinEl.setAttribute('class', 'cardTemp')
+    weatherEl.setAttribute('class', 'cardWeather')
+    humidityEl.setAttribute('class', 'cardHumidity')
+    iconEl.setAttribute('class', 'cardIcon')
 
-    iconEl.setAttribute('src',iconURL)
-    iconEl.setAttribute('alt',iconAlt)
+    iconEl.setAttribute('src', iconURL)
+    iconEl.setAttribute('alt', iconAlt)
 
     tempMaxEl.textContent = `Max Temp: ${tempMax}º F`
     tempMinEl.textContent = `Min Temp: ${tempMin}º F`
@@ -111,33 +115,45 @@ function renderCurrentContent(current, city) {
     let iconEl = document.createElement('img')
     let uviEl = document.createElement('p')
 
-    currentEl.append(cityEl,tempEl,feelsLikeEl,iconEl,weatherEl,humidityEl,uviEl)
+    currentEl.append(cityEl, tempEl, feelsLikeEl, iconEl, weatherEl, humidityEl, uviEl)
 
-    iconEl.setAttribute('src',iconUrl)
-    iconEl.setAttribute('alt',iconAlt)
-    
+    iconEl.setAttribute('src', iconUrl)
+    iconEl.setAttribute('alt', iconAlt)
+
     cityEl.textContent = `Current Weather for ${city}`
     tempEl.textContent = `Temp: ${temp}ºF`
     feelsLikeEl.textContent = `Feels Like: ${feelsLike}º F`
     humidityEl.textContent = `Humidity: ${humidity}%`
     weatherEl.textContent = `Weather: ${weather}`
     uviEl.textContent = `UV Index: ${uvi}`
-    
+
 
 }
-
 // TODO: Create Save Recent Searches
-function saveSearch(location){
- let searchHistory = JSON.parse(localStorage.getItem('WeatherAppHistory')) || []
- if(searchHistory.includes(location)===false){
-     searchHistory.push(location)
-     console.log(searchHistory)
-     localStorage.setItem('WeatherAppHistory',JSON.stringify(searchHistory))
- } 
+function saveSearch(location) {
+    let searchHistory = JSON.parse(localStorage.getItem('WeatherAppHistory')) || []
+    if (searchHistory.includes(location) === false) {
+        searchHistory.push(location)
+        console.log(searchHistory)
+        localStorage.setItem('WeatherAppHistory', JSON.stringify(searchHistory))
+    }
+    renderSearchHistory()
+}
+function renderSearchHistory() {
+    recentSearchEl.innerHTML = ''
+    var searchHistory = JSON.parse(localStorage.getItem('WeatherAppHistory')) || []
+    for (let i = 0; i < searchHistory.length; i++) {
+        var button = document.createElement('button');
+        button.setAttribute('type','button')
+        button.setAttribute('class','recent-search')
+        button.setAttribute('data-recent',searchHistory[i])
+        button.textContent = searchHistory[i];
+        recentSearchEl.append(button)
+    }
 }
 
-//  - Render all recent searches on load 
-//  - Recent searchs can be clicked to be searched again 
+renderSearchHistory()
+searchFormEl.addEventListener('submit', searchSubmitHandler)
+recentSearchEl.addEventListener('click',recentClickHandler)
 
-searchFormEl.addEventListener('submit', searchHandler)
 
