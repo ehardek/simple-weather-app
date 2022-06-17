6880cc124c007f4c2f80f27bd7aa1fc09b189606
Evan Hardek <evanhardek@gmail.com>
2022-06-16 20:35:03 -0400

    added coordsCall function

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,37 @@
-const ApiRoot = 'https://api.openweathermap.org'
-const ApiKey = '2af0ef1a0f66a04443807e8de444eb4d'
+const APIROOT = 'https://api.openweathermap.org'
+const APIKEY = '2af0ef1a0f66a04443807e8de444eb4d'
 
 const searchFormEl = document.getElementById('citySearch')
 const searchInputEl = document.getElementById('cityName')
 const recentSearchEl = document.getElementById('recentSearch')
-const currentEl = document.getElementById('current')
-const forecastEl = document.getElementById('forecast')
+const currentEl = document.getElementById('currentContainer')
+const forecastEl = document.getElementById('forecastContainer')
 
 // TODO: Create forecast fetch function
 // TODO: Create coords fetch function
-
+function coordsCall (city){
+    console.log(city)
+    var coordsUrl = `${APIROOT}/geo/1.0/direct?q=${city},US&appid=${APIKEY}`
+    console.log(coordsUrl)
+    fetch(coordsUrl)
+      .then((res)=>{return res.json()})
+      .then((data)=>{
+          if(!data){
+              alert('Your city was not found.')
+          } else {console.log(data)}
+      })
+      .catch((err)=>{console.error(err)})
+}
 // TODO: Create event handlers
 // - Search Input Handler
 function searchHandler (e){
     if(!searchInputEl.value){
-        return;
+        return alert('Please Enter A CityName');
     }
     e.preventDefault()
     var cityName = searchInputEl.value.trim();
     console.log(cityName)
+    coordsCall(cityName)
     searchInputEl.value = ''
 }
 // - Recent search click handler 
